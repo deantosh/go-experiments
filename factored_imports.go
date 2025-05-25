b50078071d87b45628cfdb63835944ac361c172c
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2025-05-25 05:19:52 -0400

    task 1: use of factored import statement

---
+++ factored_imports.go
@@ -0,0 +1,12 @@
+// Description: this go program demonstrates the use of a factored import statement.
+// Format specifier used.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
